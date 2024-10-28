236727456e26c8489a395725214ca7d41e3255cc
DariusRupsysKTU <124728932+DariusRupsysKTU@users.noreply.github.com>
2024-10-28 23:02:50 +0200

    Finished fireplace

--- scripts/quiver.gd
+++ scripts/quiver.gd
@@ -1,13 +1,43 @@
 extends Node3D
 @onready var pickable_object: XRToolsPickable = $PickableObject
 @onready var pickable_object_2: XRToolsPickable = $PickableObject2
+@onready var prakuros: XRToolsPickable = $Prakuros
+@onready var fire_wood_1: XRToolsPickable = $FireWood1
+@onready var fire_wood_2: XRToolsPickable = $FireWood2
+@onready var fire_wood_3: XRToolsPickable = $FireWood3
+@onready var fire_wood_4: XRToolsPickable = $FireWood4
+@onready var fire_wood_5: XRToolsPickable = $FireWood5
+@onready var fire_wood_6: XRToolsPickable = $FireWood6
+@onready var fire_wood_7: XRToolsPickable = $FireWood7
+@onready var fire_wood_8: XRToolsPickable = $FireWood8
+
+
 
 var arrow_instances := []
 var arrow_counter := 0
 var amount = 4
 
 func _ready():
-	pickable_object_2.visible = false;
+	pickable_object_2.visible = false
+	pickable_object_2.process_mode = Node.PROCESS_MODE_DISABLED
+	prakuros.visible = false
+	prakuros.process_mode = Node.PROCESS_MODE_DISABLED
+	fire_wood_1.visible = false
+	fire_wood_1.process_mode = Node.PROCESS_MODE_DISABLED
+	fire_wood_2.visible = false
+	fire_wood_2.process_mode = Node.PROCESS_MODE_DISABLED
+	fire_wood_3.visible = false
+	fire_wood_3.process_mode = Node.PROCESS_MODE_DISABLED
+	fire_wood_4.visible = false
+	fire_wood_4.process_mode = Node.PROCESS_MODE_DISABLED
+	fire_wood_5.visible = false
+	fire_wood_5.process_mode = Node.PROCESS_MODE_DISABLED
+	fire_wood_6.visible = false
+	fire_wood_6.process_mode = Node.PROCESS_MODE_DISABLED
+	fire_wood_7.visible = false
+	fire_wood_7.process_mode = Node.PROCESS_MODE_DISABLED
+	fire_wood_8.visible = false
+	fire_wood_8.process_mode = Node.PROCESS_MODE_DISABLED
 
 func _process(delta):
 	pass
@@ -19,9 +49,54 @@ func _on_area_3d_body_exited(body: Node3D) -> void:
 	if body == pickable_object:
 		if body.freeze:
 			body.freeze = false
-			print(body.name)
-			pickable_object_2.visible = true;
+			pickable_object_2.visible = true
+			pickable_object_2.process_mode = Node.PROCESS_MODE_ALWAYS
 	if body == pickable_object_2:
 		if body.freeze:
 			body.freeze = false
-			print(body.name)
+			fire_wood_1.visible = true
+			fire_wood_1.process_mode = Node.PROCESS_MODE_ALWAYS
+	if body == fire_wood_1:
+		if body.freeze:
+			body.freeze = false
+			fire_wood_2.visible = true
+			fire_wood_2.process_mode = Node.PROCESS_MODE_ALWAYS
+	if body == fire_wood_2:
+		if body.freeze:
+			body.freeze = false
+			fire_wood_3.visible = true
+			fire_wood_3.process_mode = Node.PROCESS_MODE_ALWAYS
+	if body == fire_wood_3:
+		if body.freeze:
+			body.freeze = false
+			fire_wood_4.visible = true
+			fire_wood_4.process_mode = Node.PROCESS_MODE_ALWAYS
+	if body == fire_wood_4:
+		if body.freeze:
+			body.freeze = false
+			fire_wood_5.visible = true
+			fire_wood_5.process_mode = Node.PROCESS_MODE_ALWAYS
+	if body == fire_wood_5:
+		if body.freeze:
+			body.freeze = false
+			fire_wood_6.visible = true
+			fire_wood_6.process_mode = Node.PROCESS_MODE_ALWAYS
+	if body == fire_wood_6:
+		if body.freeze:
+			body.freeze = false
+			fire_wood_7.visible = true
+			fire_wood_7.process_mode = Node.PROCESS_MODE_ALWAYS
+	if body == fire_wood_7:
+		if body.freeze:
+			body.freeze = false
+			fire_wood_8.visible = true
+			fire_wood_8.process_mode = Node.PROCESS_MODE_ALWAYS
+	if body == fire_wood_8:
+		if body.freeze:
+			body.freeze = false
+			prakuros.visible = true
+			prakuros.process_mode = Node.PROCESS_MODE_ALWAYS
+	if body == prakuros:
+		if body.freeze:
+			body.freeze = false
+			print("paimtos prakuros")
--- scripts/titnago_collision.gd
+++ scripts/titnago_collision.gd
@@ -1,5 +1,6 @@
 extends CollisionShape3D
 @onready var gpu_particles_3d: GPUParticles3D = $"../../GPUParticles3D"
+@onready var prakuros: XRToolsPickable = $"../../../Prakuros"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +14,10 @@ func _process(delta: float) -> void:
 func _on_area_3d_area_exited(area: Area3D) -> void:
 	if area.get_parent().name == "PickableObject":
 		gpu_particles_3d.restart()
+		var particles_pos = gpu_particles_3d.global_position
+		var prakuros_pos = prakuros.global_position
+		if prakuros_pos.y < particles_pos.y and prakuros_pos.y+2 > particles_pos.y :
+			if particles_pos.x-1 < prakuros_pos.x and prakuros_pos.x < particles_pos.x+1:
+				if particles_pos.z-1 < prakuros_pos.z and prakuros_pos.z < particles_pos.z+1:
+					print("Ugnis!")
 	pass # Replace with function body.
