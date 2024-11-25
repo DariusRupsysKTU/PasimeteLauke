8e41e62264e789a84c115a55af326a330945980e
jaga1mo <justinais321.JP@gmail.com>
2024-11-25 23:14:14 +0200

    trying cart

---
+++ scripts/cart_holding.gd
@@ -0,0 +1,40 @@
+@tool
+extends Area3D
+
+## Signal when pointer event occurs on area
+signal pointer_event(event)
+
+var bodies: Dictionary
+var flag := false
+
+func _process(delta: float) -> void:
+	#super._process(delta)
+	for body in bodies:
+		if not body.freeze:
+			if bodies[body] != null and body.transform.is_equal_approx(bodies[body]):
+				#body.collision_layer = 0
+				body.freeze = true
+				body.freeze_mode = RigidBody3D.FREEZE_MODE_STATIC
+				flag=true
+				await body.reparent(self)
+				flag = false
+
+			bodies[body] = Transform3D(body.transform)
+
+func _on_body_entered(body: Node3D) -> void:
+	print(body.name)
+	if body != self:
+		bodies[body] = Transform3D(body.transform)
+
+func _on_body_exited(body: Node3D) -> void:
+	if(flag):
+		flag = false
+		pass
+	print(body.name)
+	bodies[body] = null
+	body.freeze_mode = RigidBody3D.FREEZE_MODE_KINEMATIC
+	body.freeze = false
+	var wn = get_node("/root/world")
+	print(wn.name)
+	body.reparent(wn, true)
+	#body.collision_layer = 3
