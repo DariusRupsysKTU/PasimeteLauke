73db7a979a1daea907bdcf70671ca1aeb60f76b9
DariusRupsysKTU <124728932+DariusRupsysKTU@users.noreply.github.com>
2024-11-26 13:05:40 +0200

    Bloga

--- scripts/eating.gd
+++ scripts/eating.gd
@@ -1,6 +1,7 @@
 extends Area3D
 @onready var area_3d: Area3D = $"."
 @onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
+@onready var mesh_instance_3d: MeshInstance3D = $"../MeshInstance3D"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,3 +18,5 @@ func _on_body_entered(body: Node3D) -> void:
 	body.process_mode = Node.PROCESS_MODE_DISABLED
 	body.hide()
 	audio_stream_player_3d.play()
+	if body.name.begins_with("PoisonousBerry"):
+		mesh_instance_3d.set_instance_shader_parameter("blur_power", 0.005)
