1d6c46c3591b8dce5d4743886730742dd761571a
DariusRupsysKTU <124728932+DariusRupsysKTU@users.noreply.github.com>
2024-11-26 12:15:57 +0200

    Rabbit spawner

---
+++ scripts/rabbit.gd
@@ -0,0 +1,20 @@
+extends CharacterBody3D
+@onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+
+
+var SPEED = 3.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	var current_location = global_transform.origin
+	var next_location = navigation_agent_3d.get_next_path_position()
+	var new_velocity = (next_location - current_location).normalized() * SPEED
+	velocity = new_velocity
+	move_and_slide()
+
+func update_target_location(target_location):
+	navigation_agent_3d.target_position = target_location
---
+++ scripts/rabbit_hole.gd
@@ -0,0 +1,22 @@
+extends Node3D
+@onready var rabbit = preload("res://Nodes/rabbit.tscn")
+@onready var trap: Node3D = $"../Trap"
+
+var timer := 10.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if timer > 0:
+		timer -= delta
+	else:
+		var rabbit_instance = rabbit.instantiate()
+		rabbit_instance.position = position
+		get_parent().add_child(rabbit_instance)
+		rabbit_instance.update_target_location(trap.position)
+		timer = 60
+		
