8a434d0bd4c5cc656eb9636def77a5fd5c36f2ee
jaga1mo <justinais321.JP@gmail.com>
2024-12-09 23:34:54 +0200

    poison

--- scripts/eating.gd
+++ scripts/eating.gd
@@ -2,8 +2,11 @@ extends Area3D
 @onready var area_3d: Area3D = $"."
 @onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
 @onready var mesh_instance_3d: MeshInstance3D = $"../MeshInstance3D"
+@onready var fade: XRToolsFade = $"../Fade"
+@onready var xr_camera_3d: XRCamera3D = $".."
 
 
+var timer = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,7 +14,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if timer>0:
+		timer-=delta
+		fade.set_fade(xr_camera_3d, Color(0.7, 0.4, 0, timer*0.25))
+	else:
+		fade.set_fade(xr_camera_3d, Color(0, 0, 0, 0))
 
 func _on_body_entered(body: Node3D) -> void:
 	print(body.name)
@@ -19,4 +26,5 @@ func _on_body_entered(body: Node3D) -> void:
 	body.hide()
 	audio_stream_player_3d.play()
 	if body.name.begins_with("PoisonousBerry"):
-		mesh_instance_3d.set_instance_shader_parameter("blur_power", 0.005)
+		timer = 10
+		fade.set_fade(xr_camera_3d, Color(0.7, 0.4, 0, timer*0.25))
