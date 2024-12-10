f0e81f1cf6f312f3d95474fa161a7541530c6044
jaga1mo <justinais321.JP@gmail.com>
2024-12-10 12:38:42 +0200

    adjusted signs

--- scripts/rabbit_hole.gd
+++ scripts/rabbit_hole.gd
@@ -1,9 +1,10 @@
 extends Node3D
 @onready var rabbit = preload("res://Nodes/rabbit.tscn")
 @onready var trap: Node3D = $"../Trap2"
+@onready var trap_area: Area3D = $"../Trap2/Area3D3"
 
-var timer := 100.0
-
+var timer = 15
+var rabbit_ready = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,12 +12,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if timer > 0:
-		timer -= delta
-	else:
-		var rabbit_instance = rabbit.instantiate()
-		rabbit_instance.position = position
-		get_parent().add_child(rabbit_instance)
-		rabbit_instance.update_target_location(trap.position)
-		timer = 60
-		
+	if trap_area.active:
+		if timer > 0:
+			timer -= delta
+			rabbit_ready = true
+		else:
+			rabbit_ready = false
+			var rabbit_instance = rabbit.instantiate()
+			rabbit_instance.position = position
+			get_parent().add_child(rabbit_instance)
+			rabbit_instance.update_target_location(trap.position)
+			timer = 30
